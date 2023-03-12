4bfabe361ded498be537dfd5b4843024c5772ae6
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-12 21:04:28 +0200

    height

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -4,10 +4,11 @@ export const ContainerBox = styled.div`
   margin-left: auto;
   margin-right: auto;
   width: 290px;
-  min-height: 625px;
+  min-height: 550px;
 
   @media screen and (min-width: 768px) {
     width: 738px;
+    min-height: 625px;
   }
   @media screen and (min-width: 1200px) {
     width: 1170px;
