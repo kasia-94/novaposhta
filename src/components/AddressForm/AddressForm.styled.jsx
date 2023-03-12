89f2f4b7e8407386076c83d0cd72f24f4c4b90c0
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-12 18:27:21 +0200

    delete padding

--- src/components/AddressForm/AddressForm.styled.jsx
+++ src/components/AddressForm/AddressForm.styled.jsx
@@ -17,8 +17,3 @@ export const ButtonSubmit = styled(Button)`
 export const Input = styled(TextField)`
   width: 210px;
 `;
-
-export const Form = styled.form`
-  display: flex;
-  flex-direction: row;
-`;
