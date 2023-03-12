d4c8e558385f548fa29fc54eef7f6f343464b1e7
Kateryna Berehovych <kasia-94@ukr.net>
2023-03-12 20:42:55 +0200

    Add footer

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,9 @@
+import { ErrorBox } from './ErrorPage.styled';
+
 export default function ErrorPage() {
   return (
-    <>
+    <ErrorBox>
       <>Опаньки... щось пішло не так</>
-    </>
+    </ErrorBox>
   );
 }
